1f560f58da006800db17b5b3bbfc7122a2f4e916
alejahndro <alejahndro02@gmail.com>
2018-09-07 06:47:32 -0500

    boton para mostrar y ocultar formulario para agregar usuarios

--- js/main.js
+++ js/main.js
@@ -66,3 +66,19 @@ $("#add").click(function(evento){
 // $("#del").click(function(){
 //     alert( "Funciona" );
 // }
+$(document).ready(function(){
+    $("#add").click(function () {
+       $("#form").each(function() {
+         displaying = $(this).css("display");
+         if(displaying == "block") {
+           $(this).fadeOut('slow',function() {
+            $(this).css("display","none");
+           });
+         } else {
+           $(this).fadeIn('slow',function() {
+             $(this).css("display","block");
+           });
+         }
+       });
+     });
+   });
